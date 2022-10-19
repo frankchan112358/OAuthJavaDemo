abec2305d668a548f98b9efc6dba4efa62b20e61
Frank Chan <frank.chan@hy-tech.com.tw>
2022-10-19 17:39:03 +0800

    init

---
+++ src/main/java/com/frankchan/oauthjavademo/OAuthJavaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.frankchan.oauthjavademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OAuthJavaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OAuthJavaDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frankchan/oauthjavademo/OAuthJavaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankchan.oauthjavademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthJavaDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
